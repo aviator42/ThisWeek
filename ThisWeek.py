53fcfe9d5b102c563523da0a8db9677335160008
aviator42 <migu3l.r4mon@gmail.com>
2019-10-07 15:45:15 -0500

    part one of today

--- ThisWeek.py
+++ ThisWeek.py
@@ -5,3 +5,34 @@ y = "those who know %s and those who %S" (binary, doNot)
 
 print(x)
 print(y)
+
+print("I said: %r.:" % x)
+print("i also said: '%s'." % y)
+
+hilarious = true
+jokeEvaluation = "isn't that joke so funny!? &r"
+
+print(jokeEvaluation % hilarious)
+
+w = "this is the left side of..."
+e = "a string with a right side."
+print(w+e)
+
+#more fun
+print("Mary had a little lamb.")
+print("Its fleece was white as %s" % 'snow')
+print("And everywhere that Mary wnet.")
+print("." * 10)
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
