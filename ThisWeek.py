afd17bb4585b98b155120d26ba3abfb217f1eb72
aviator42 <migu3l.r4mon@gmail.com>
2019-10-30 15:45:20 -0500

    part one of today

--- ThisWeek.py
+++ ThisWeek.py
@@ -1,7 +1,7 @@
 x = "There are %d types of people." %10
 binary = "binary"
 doNot = "don't"
-y = "those who know %s and those who %S" (binary, doNot)
+y = "Those who know %s and those who %s" % (binary, doNot)
 
 print(x)
 print(y)
@@ -9,7 +9,7 @@ print(y)
 print("I said: %r.:" % x)
 print("i also said: '%s'." % y)
 
-hilarious = true
+hilarious = True
 jokeEvaluation = "isn't that joke so funny!? &r"
 
 print(jokeEvaluation % hilarious)
@@ -36,8 +36,84 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-print()
-print()
+print(end1 + end2 + end3 + end4 + end5 + end6)
+print(end7 + end8 + end9 + end10 + end11 + end12)
 
+# But wait! There's more:
 formatter = "%r %r %r %r"
-print
+print(formatter % (1, 2, 3, 4))
+print(formatter % ("one", "two", "three", "four"))
+print(formatter % (True, False, False, True))
+print(formatter % (formatter, formatter, formatter, formatter))
+
+# why do I use %r instead of %s in the above example?
+
+# which should I use on a regular basis?
+
+# why does %r sometimes give me single quotes around things?
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+
+# examine closely the differences between the %r formatter and %s formatter
+print("Here are the months: %r" % months)
+print("Here are the months: %s" % months)
+
+# escape sequences redux
+tabbyCat = "\tI'm tabbed in."
+persianCat = "I'm split\non a line."
+backslashCat = "I'm \\ a \\ cat."
+topCat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabbyCat)
+print(persianCat)
+print(backslashCat)
+print(topCat)
+
+# Escape Seq            What it does?
+# \\
+# \'
+# \"
+# \a
+# \b
+# \f
+# \n
+# \N{name}
+# \r
+# \t
+# \uxxxx
+# \Uxxxxxxxx
+# \v
+# \ooo
+# \xhh
+
+# What does the following code block do:
+#   while True:
+#       for i in ["/", "-", "|", "\\", "|"]:
+#           print("%s\r" % i, end='')
+
+#  Can you use ''' instead of """ ?
+
+age = input("How old are you?")
+height = input("How tall are you?")
+
+print("So, you are %r old and %r tall." % (age, height))
+
+# Ask 4 more questions and handle those responses
+
+
